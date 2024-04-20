a2d15c2412122994a14a6e1788f5950328363787
AnarchyGhost <k.markelov@anarchyghost.com>
2024-04-20 21:30:52 +0300

    CORS Settings

---
+++ server/src/main/kotlin/com/anarchyghost/plugins/CORS.kt
@@ -0,0 +1,12 @@
+package com.anarchyghost.plugins
+
+import io.ktor.http.*
+import io.ktor.server.application.*
+import io.ktor.server.plugins.cors.routing.*
+
+fun Application.configureCORS() {
+    install(CORS) {
+        anyHost()
+        allowHeader(HttpHeaders.ContentType)
+    }
+}
